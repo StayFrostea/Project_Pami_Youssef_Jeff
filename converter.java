9536d1d53e03db908309377007f174cb271a916e
StayFrostea <59980526+StayFrostea@users.noreply.github.com>
2020-01-17 14:38:36 -0700

    Update converter.java

--- converter.java
+++ converter.java
@@ -1,1 +1,28 @@
 //test
+
+public class Converter {
+    /*
+
+    @Author: Jeff Roszell
+    @Author: 
+    @Author:
+
+    */
+
+    private double celsiusToFarenheit(double C)
+    {
+
+    }
+
+    private double farenheitToCelsius(double F)
+    {
+
+    }
+
+    public static void main(String [] args)
+    {
+
+    }
+
+    
+}
