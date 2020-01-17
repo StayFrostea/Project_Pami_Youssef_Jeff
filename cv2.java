1eef7033e84ae258d6998f7ce78c497ac4aae9c7
YousseMk <youssef.abdelmaksoud@ucalgary.ca>
2020-01-17 15:02:58 -0700

    first commit

---
+++ cv2.java
@@ -0,0 +1,29 @@
+//test
+
+public class Converter {
+    /*
+
+    @Author: Jeff Roszell
+    @Author: Jesupamilerin Falade
+    @Author:
+
+    */
+
+    private double celsiusToFarenheit(double C)
+    {
+
+    }
+
+    private double farenheitToCelsius(double F)
+    {
+
+    }
+
+    public static void main(String [] args)
+    {
+        println(celciusToFarenheit(180));
+        println(farenheitToCelsius(250));
+    }
+
+    //help
+}
