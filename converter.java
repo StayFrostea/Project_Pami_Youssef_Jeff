789b28255aa679bde7287e4c06c63fcb8dc3c2cf
StayFrostea <59980526+StayFrostea@users.noreply.github.com>
2020-01-18 18:18:53 -0700

    modified:   converter.java 	deleted:    cv2.java

--- converter.java
+++ converter.java
@@ -9,18 +9,18 @@ public class Converter {
      * 
      */
 
-    private double celsiusToFarenheit(double C) {
-        double result = (C * (9 / 5)) + 32;
+    private double celsiusToFarenheit(final double C) {
+        final double result = (C * (9 / 5)) + 32;
         return result;
     }
 
-    private double farenheitToCelsius(double F) {
-        double result = (F * (5 / 9)) - 32;
+    private double farenheitToCelsius(final double F) {
+        final double result = (F * (5 / 9)) - 32;
         return result;
     }
 
-    public static void main(String[] args) {
-        println(celciusToFarenheit(180));
-        println(farenheitToCelsius(250));
+    public static void main(final String[] args) {
+        System.out.println(celciusToFarenheit(180));
+        System.out.println(farenheitToCelsius(250));
     }
 }
