163be9e4ee16a5f46168ff979052a1fecccd47b7
PamiFalade <60018401+PamiFalade@users.noreply.github.com>
2020-01-23 11:31:38 -0700

    Update converter.java

--- converter.java
+++ converter.java
@@ -10,16 +10,6 @@ public class Converter {
      * 
      */
 
-<<<<<<< HEAD
-    public double celsiusToFarenheit(final double C) {
-        final double result = (C * (9 / 5)) + 32;
-        return result;
-    }
-
-    public double farenheitToCelsius(final double F) {
-        final double result = (F * (5 / 9)) - 32;
-        return result;
-=======
     
     private double celciusToFarenheit(double c){
           return (c * (1.8)) + 32;
@@ -27,32 +17,25 @@ public class Converter {
     
     private double farenheitToCelcius(double f){
         return (f - 32) * (0.56);
->>>>>>> e35a13d09d5241cefd1e06e99b947253514f4564
     }
 
     private double kilometersToMiles(final double M)
     {
         return (M*0.621371);
     }
-<<<<<<< HEAD
-
-    public static void main(final String[] args) {
-        Converter temp = new Converter();
-        System.out.println(temp.celciusToFarenheit(180));
-        System.out.println(temp.farenheitToCelsius(250));
-=======
     
     public static void main(String[] args) {
         Converter convVal = new Converter();
     
         double f = convVal.celciusToFarenheit(180);
         double c = convVal.farenheitToCelcius(250);
+        double m = convVal.kilometersToMiles(100);
     
         System.out.print(c);
         System.out.println(" " + "degrees C");
         System.out.print(f);
         System.out.print(" " + "degrees F");
->>>>>>> e35a13d09d5241cefd1e06e99b947253514f4564
+        System.out.println("100 km in miles is " + m);
     }
 }
     
