2cc98d2fadc95e839296178cff3e2239dc2ddc49
PamiFalade <60018401+PamiFalade@users.noreply.github.com>
2020-01-23 11:42:03 -0700

    Create converter.test.java

---
+++ converter.test.java
@@ -0,0 +1,41 @@
+package com.company;
+public class Converter {
+    /*
+     * 
+     * @Author: Jeff Roszell
+     * 
+     * @Author: Jesupamilerin Falade
+     * 
+     * @Author: Youssef Maksoud
+     * 
+     */
+
+    
+    private double celciusToFarenheit(double c){
+          return (c * (1.8)) + 32;
+    }
+    
+    private double farenheitToCelcius(double f){
+        return (f - 32) * (0.56);
+    }
+
+    private double kilometersToMiles(final double M)
+    {
+        return (M*0.621371);
+    }
+    
+    public static void main(String[] args) {
+        Converter convVal = new Converter();
+    
+        double f = convVal.celciusToFarenheit(180);
+        double c = convVal.farenheitToCelcius(250);
+        double m = convVal.kilometersToMiles(100);
+    
+        System.out.print(c);
+        System.out.println(" " + "degrees C");
+        System.out.print(f);
+        System.out.print(" " + "degrees F");
+        System.out.println("100 km in miles is " + m);
+    }
+}
+    
